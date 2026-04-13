2cd78ccf6651148091e8b34cac48ac659035fd17
Jon Rafkind <jon@rafkind.com>
2026-04-12 21:22:13 -0700

    draw clouds. fix texture coordinate offset

--- main.go
+++ main.go
@@ -1,25 +1,54 @@
 package main
 
 import (
+    _ "fmt"
     "os"
+    "image"
+    "image/color" 
+    "sync"
+    "math/rand/v2"
     _ "image/png"
+    _ "image/jpeg"
     "github.com/hajimehoshi/ebiten/v2"
     "github.com/hajimehoshi/ebiten/v2/inpututil"
     "github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-func draw(screen *ebiten.Image, planetImage *ebiten.Image, timeSeconds uint64, shader *ebiten.Shader) {
+func draw(screen *ebiten.Image, planetImage *ebiten.Image, cloudImage *ebiten.Image, timeSeconds float64, shader *ebiten.Shader) {
+
+    /*
+    var opts2 ebiten.DrawImageOptions
+    opts2.GeoM.Translate(10, 200)
+    opts2.GeoM.Scale(0.5, 0.5)
+    screen.DrawImage(cloudImage, &opts2)
+    */
 
     bounds := planetImage.Bounds()
     w, h := bounds.Dx(), bounds.Dy()
 
     opts := &ebiten.DrawRectShaderOptions{}
+    opts.GeoM.Translate(10, 100)
+    /*
+    opts.GeoM.Scale(0.5, 0.5)
+    */
+
+    // fmt.Printf("Resolution: %v x %v\n", w, h)
+
+    rotationSpeed := timeSeconds / 300.0
+
     opts.Uniforms = map[string]interface{}{
-        "Time":       float32(float64(timeSeconds) / 200.0),
+        "Time":       float32(rotationSpeed),
+        // "Time":       float32(0),
         "Resolution": []float32{float32(w), float32(h)},
     }
     opts.Images[0] = planetImage
 
+    screen.DrawRectShader(w, h, shader, opts)
+
+    opts.Blend = ebiten.BlendLighter
+    opts.Images[0] = cloudImage
+    opts.ColorScale.ScaleAlpha(0.2)
+    opts.Uniforms["Time"] = float32(rotationSpeed * 1.5)
     screen.DrawRectShader(w, h, shader, opts)
 }
 
@@ -27,6 +56,47 @@ type Game struct {
     Counter uint64
     Shader *ebiten.Shader
     Planet *ebiten.Image
+    CloudImage *ebiten.Image
+    Init sync.Once
+    drawClouds int
+}
+
+func makeCloudImage(bounds image.Rectangle) *ebiten.Image {
+    w, h := bounds.Dx(), bounds.Dy()
+    img := ebiten.NewImage(w, h)
+    // img.Fill(color.RGBA{B: 255, A: 255})
+
+    /*
+    if 2 > 1 {
+        img2, _, _ := ebitenutil.NewImageFromFile("rect2.png")
+        if 2 > 1 {
+            return img2
+        }
+        var opts ebiten.DrawImageOptions
+        img.DrawImage(img2, &opts)
+
+        return img
+    }
+    */
+
+    clouds := []string{"cloud1.png", "cloud-a.png"}
+
+    for _, cloudFile := range clouds {
+        cloud, _, err := ebitenutil.NewImageFromFile(cloudFile)
+        if err != nil {
+            panic(err)
+        }
+
+        cloudBounds := cloud.Bounds()
+
+        for range 10 {
+            var opts ebiten.DrawImageOptions
+            opts.GeoM.Translate(float64(w - cloudBounds.Dx()) * rand.Float64(), float64(h - cloudBounds.Dy()) * rand.Float64())
+            img.DrawImage(cloud, &opts)
+        }
+    }
+
+    return img
 }
 
 func MakeGame() *Game {
@@ -39,20 +109,25 @@ func MakeGame() *Game {
         panic(err)
     }
 
-    planetImage, _, err := ebitenutil.NewImageFromFile("world1.png")
+    // planetImage, _, err := ebitenutil.NewImageFromFile("rect2.png")
+    planetImage, _, err := ebitenutil.NewImageFromFile("mars.jpg")
     if err != nil {
         panic(err)
     }
+
+    cloudImage := makeCloudImage(planetImage.Bounds())
 
     return &Game{
         Counter: 0,
         Shader: shader,
         Planet: planetImage,
+        drawClouds: 800,
+        CloudImage: cloudImage,
     }
 }
 
 func (g *Game) Update() error {
-    g.Counter++
+    g.Counter += 1
 
     keys := inpututil.AppendJustPressedKeys(nil)
     for _, k := range keys {
@@ -65,14 +140,28 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-    draw(screen, g.Planet, g.Counter, g.Shader)
+    screen.Fill(color.NRGBA{R: 32, G: 0, B: 0, A: 255})
+
+    // if g.drawClouds > 0 {
+        draw(screen, g.Planet, g.CloudImage, float64(g.Counter), g.Shader)
+    // }
+
+    /*
+    var opts ebiten.DrawImageOptions
+    opts.GeoM.Translate(10, 200)
+    opts.GeoM.Scale(0.5, 0.5)
+    screen.DrawImage(g.CloudImage, &opts)
+    */
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-    return g.Planet.Bounds().Dx(), g.Planet.Bounds().Dy()
+    // return g.Planet.Bounds().Dx(), g.Planet.Bounds().Dy()
+    return outsideWidth, outsideHeight
 }
 
 func main() {
 
+    ebiten.SetWindowSize(1024, 768)
+    ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
     ebiten.RunGame(MakeGame())
 }
